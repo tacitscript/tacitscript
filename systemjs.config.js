a32d62d24190fbc4e4b997254cb6ec874c87fd72
Darryl Morris <darryl.morris@darktrace.com>
2020-08-29 21:29:39 +0100

    fixes to test runner

--- systemjs.config.js
+++ systemjs.config.js
@@ -7,7 +7,7 @@
     }, 
     map: {
       // babel transpiler
-      'plugin-babel-tacitscript': 'plugin-babel-0.0.25-tacitscript.js',
+      'plugin-babel-tacitscript': 'src/plugin-babel-0.0.25-tacitscript.js',
       'systemjs-babel-build': 'npm:systemjs-plugin-babel@0.0.25/systemjs-babel-browser.js',
     },
     meta: {
