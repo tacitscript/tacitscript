5c1414947d89c8b50687ecd9445f65f47db8576d
Darryl Morris <darryl.morris@darktrace.com>
2020-08-30 16:20:47 +0100

    add remote references

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -3,11 +3,13 @@
     transpiler: 'plugin-babel-tacitscript',
     paths: {
       "npm:": 'https://unpkg.com/',
+      "github:": "https://raw.githubusercontent.com/tacitscript/tacitscript/master/src/",
     }, 
     map: {
       // babel transpiler
       'plugin-babel-tacitscript': 'src/plugin-babel-0.0.25-tacitscript.js',
       'systemjs-babel-build': 'npm:systemjs-plugin-babel@0.0.25/systemjs-babel-browser.js',
+      'tacitscript': "github:tacitscript.js",
     },
     meta: {
     '*.jsx': {
--- src/test.js
+++ src/test.js
@@ -1,5 +1,5 @@
 const {expect, assert} = chai;
-import ts from "./tacitscript.js";
+import ts from "tacitscript";
 
 ///==========================================================
 // functional utilites
