4ee4f16a1c79d0198285a12e240583e98cb19f54
Darryl Morris <darryl.morris@darktrace.com>
2020-09-12 15:59:17 +0100

    add lesson 1 verifications

--- tutorial.js
+++ tutorial.js
@@ -1,6 +1,37 @@
 const {expect, assert} = chai;
 import ts from "tacitscript";
 
+
+describe("Lesson 1", () => {
+	const jsVariable = 4;
+
+	/*ts
+		number		3		// this is a number
+
+		decimal		3.5		also a number. NB: terminating line comments need not be prefixed with //
+
+		string		"strings may continue
+over several lines"			strings are delimited by double-quotes <i>only</i>
+
+		falsey		()		there is only one falsey value, <strong>undefined</strong>, denoted ()
+
+		array		(1 "hello"
+				() number)	arrays are bounded by brackets and delimited by any kind of whitespace
+
+		expression	2*jsVariable	js variables in scope may be directly referenced within ts block
+
+		pitfall		4 + number	NB: tokens <i>cannot</i> be freely separated by whitespace
+	*/
+
+	it("number equals 3", () => expect(number).eql(3));
+	it("decimal equals 3.5", () => expect(decimal).eql(3.5));
+	it('string equals "strings may continue\\nover several lines"', () => expect(string).eql("strings may continue\nover several lines"));
+	it("falsey equals undefined", () => expect(falsey).eql(undefined));
+	it('array equals [1, "hello", undefined, 3]', () => expect(array).eql([1, "hello", undefined, 3]));
+	it("expression equals 8", () => expect(expression).eql(8));
+	it("pitfall equals 4", () => expect(pitfall).eql(4));
+});
+
 ///==========================================================
 // functional utilites
 
