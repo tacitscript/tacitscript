5c0e6067df5be989ab415c740030a561577e31cc
Darryl Morris <darryl.morris@darktrace.com>
2020-09-20 17:59:27 +0100

    intermediate commit - trying to support tutorial build

--- tutorial/config.js
+++ tutorial/config.js
@@ -1,17 +1,19 @@
 (function (global) {
   System.config({
     transpiler: 'plugin-babel-tacitscript',
-    globalName: "tutorial",
+    paths: {
+      "cdnjs:": "https://cdnjs.cloudflare.com/ajax/libs/",
+      "npm:": "https://unpkg.com/",
+    },
     map: {
       // babel transpiler
       'plugin-babel-tacitscript': 'common/lib/plugin-babel_c72965b.js',
       'systemjs-babel-build': "common/lib/systemjs-babel-browser_c72965b-sloppy.js",
       "tacitscript": "common/src/tacitscript.js",
-      "react": "https://cdnjs.cloudflare.com/ajax/libs/react/16.8.1/umd/react.development.js",
-      "react-dom": "https://unpkg.com/react-dom@16.8.1/umd/react-dom.development.js",
-      "material-ui": "https://unpkg.com/@material-ui/core@4.11.0/umd/material-ui.development.js",
-      "history": "https://cdnjs.cloudflare.com/ajax/libs/history/4.7.2/history.js",
-      "mocha": "https://cdnjs.cloudflare.com/ajax/libs/mocha/1.13.0/mocha.min.js",
+      "react": "cdnjs:react/16.8.1/umd/react.development.js",
+      "react-dom": "npm:react-dom@16.8.1/umd/react-dom.development.js",
+      "material-ui": "npm:@material-ui/core@4.11.0/umd/material-ui.development.js",
+      "history": "common/lib/history_4.7.2.js",
     },
     babelOptions: {
       modularRuntime: false,
