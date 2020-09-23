db34abd1b76ec0091b7f457e557e46ebd47ffa03
Darryl Morris <darryl.morris@darktrace.com>
2020-09-23 18:04:43 +0100

    get rid of repaint glitch

--- tutorial/components/exercise1.js
+++ tutorial/components/exercise1.js
@@ -14,19 +14,21 @@ export default ({}) => {
 	useEffect(() => {
 		document.getElementById("exercise-1-tests").innerHTML = "";
 
-		describe("Lesson 1 Exercise Tests", () => {
-			eval(ts2es6(R.flatten(test1(answer || "()")).join("\n")).replace(/const /g, "var "));
+		setTimeout(() => {
+			describe("Lesson 1 Exercise Tests", () => {
+				eval(ts2es6(R.flatten(test1(answer || "()")).join("\n")).replace(/const /g, "var "));
 
-			it('"numbers" is an array', () => expect(Array.isArray(numbers)).eql(true));
-			it('"numbers" has more than one element', () => expect(numbers.length > 1).eql(true));
-			it('all elements of "numbers" are numbers', () => expect(numbers.every(number => typeof(number) === "number")));
-			it('sum of elements in "numbers" equals 20', () => expect(numbers.reduce((a, b) => a + b)).eql(20));
-		});
+				it('"numbers" is an array', () => expect(Array.isArray(numbers)).eql(true));
+				it('"numbers" has more than one element', () => expect(numbers.length > 1).eql(true));
+				it('all elements of "numbers" are numbers', () => expect(numbers.every(number => typeof(number) === "number")));
+				it('sum of elements in "numbers" equals 20', () => expect(numbers.reduce((a, b) => a + b)).eql(20));
+			});
 
-		mocha.setup({
-			grep: /^Lesson 1 Exercise Tests/,
-		});
-		mocha.run(null, "exercise-1-tests");
+			mocha.setup({
+				grep: /^Lesson 1 Exercise Tests/,
+			});
+			mocha.run(null, "exercise-1-tests");},
+		500);
 	}, [answer]);
 
 	return 	<div className="code-block exercises">
