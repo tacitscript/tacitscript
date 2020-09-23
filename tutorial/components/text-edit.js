9653735760d7e259d2f14d705bfa7b3bf62628e2
Darryl Morris <darryl.morris@darktrace.com>
2020-09-23 15:30:43 +0100

    styling on text edit

--- tutorial/components/text-edit.js
+++ tutorial/components/text-edit.js
@@ -3,11 +3,17 @@ const {css} = Glamor;
 
 
 const style = css({
+    width: "100%",
     borderBottom: "1px solid rgba(0, 0, 0, 0.2)",
     padding: "0 !important",
+    color: "#271c19",
+    " .MuiInputBase-input": {
+        fontFamily: "Roboto Mono, monospace",
+        fontSize: "0.8rem",
+    },
 });
 
 export default ({}) => {
 
-    return <InputBase {...style} multiline/>;
+    return <InputBase {...style} inputProps={{spellCheck: false}} multiline/>;
 };
