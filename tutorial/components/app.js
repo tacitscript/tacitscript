b8f91404c213cf2681155440da116b7efc531b9e
Darryl Morris <darryl.morris@darktrace.com>
2020-09-23 10:52:26 +0100

    stubs for new sections

--- tutorial/components/app.js
+++ tutorial/components/app.js
@@ -76,43 +76,52 @@ export default ({store}) => {
 		<h1>tacitscript tutorial</h1>
 		<div className="section">
 			<p><strong>tacitscript - a language for building algorithms</strong></p>
-			<p>tacitscript is a programming language optimized for building algorithms from composable parts, using a simplified syntax. To learn more about the ideas behind tacitscript, see <a href="#appendix-a">here</a>.</p>
+			<p>tacitscript is an esolang optimized for building algorithms from concise, composable parts. To learn more about the ideas behind tacitscript, see <a href="#appendix-a">here</a>.</p>
 			<p>This tutorial will introduce the language incrementally through example and interactive exercises.</p>
 		</div>
 
 		<div className="section">
 			<h2 className="heading">Lesson 1: tacitscript (ts) Blocks</h2>
 			<hr/>
-			<p>tacitscript blocks of code are embedded within normal JavaScript code.</p>
-			<p>They are defined within special comment blocks beginning with a <span className="code">/*ts</span> and ending with a <span className="code">*/</span>. The tacitscript transpiler will convert these blocks to normal JavaScript code. Definitions are shared freely between js and ts code.</p>
+			<p>tacitscript blocks of code are embedded within JavaScript code.</p>
+			<p>They are defined within special comment blocks beginning with a <span className="code">/*ts</span> and ending with a <span className="code">*/</span>. The tacitscript transpiler will convert these blocks to standard JavaScript code. Definitions are shared freely between js and ts code.</p>
+			<p>Each ts block consists of a list of name-value bindings.</p>
 			<div className="code-block">
 				<div dangerouslySetInnerHTML={{__html: l1}}/>
 				<div id="lesson-1-tests" className="mocha"></div>
 			</div>
 			<ul>
-				<li>Each ts block consists of a list of name-value bindings.</li>
 				<li>Each ts name is made from upper and lower-case alphabetic characters <i>only</i>.</li>
-				<li>The value bound to a name is given by any valid ts expression.</li>
-				<li>Any characters on the line following a closed ts expression are considered to be a comment.</li>
+				<li>The value bound to a name can be any valid ts expression.</li>
+				<li>Any characters on the line following a closed ts expression are considered a comment.</li>
 			</ul>
 			<h3>Exercises</h3>
-			<p>Please select the file <span className="code">tutorial.js</span> in the panel to the left. This will display the interactive exercises for these lessons.</p>
-			<p>The exercises are a sequence of automated tests, whose results are output on this page on save. The first set of tests is driving the assertions made in the lesson above. The second block of tests
-				contains stubs for ts declarations, initially left undefined. Replace these null definitions with your own submissions, and click Save to see the tests run against them. (Remove the <i>x</i> before
-				the test declaration to enable it.)</p>
 			<div className="code-block exercises">
 				<li>Define an array <b>numbers</b> that contains multiple numbers, and only numbers, that add up to 20.</li>
 				<div id="lesson-1-exercise-tests" className="mocha"></div>
 			</div>
 		</div>
 		<div className="section">
-			<h2 className="heading">Lesson 2: Operators and Precedence</h2>
+			<h2 className="heading">Lesson 2: Operators</h2>
 			<hr/>
 			<p>
-				tacitscript is a language of operators. Most non-alphabetic characters have been repurposed to act as operators within a ts block.
+				tacitscript is a language of operators. Most non-alphabetic characters on the standard keyboard have been repurposed to act as operators within a ts block.
 				Operators in tacitscript are each a single character long.
 			</p>
 			<p>The standard arithmetic operators +, -, * and /, all retain their meaning in tacitscript.</p>
 		</div>
+		<div className="section">
+			<h2 className="heading">Lesson 3: Functions</h2>
+			<hr/>
+		</div>
+		<div className="section">
+			<h2 className="heading">Lesson 4: Chaining</h2>
+			<hr/>
+		</div>
+		<div className="section">
+			<h2 className="heading">Lesson 5: Higher-Order Operators</h2>
+			<hr/>
+		</div>
+
 	</div>;
 };
