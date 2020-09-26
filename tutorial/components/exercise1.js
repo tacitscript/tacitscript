448cb6f5de9f289d9b18304eb108b086668d9411
Darryl Morris <darryl.morris@darktrace.com>
2020-09-26 12:47:54 +0100

    catch syntax error + dont run tests when empty or syntax error

--- tutorial/components/exercise1.js
+++ tutorial/components/exercise1.js
@@ -10,18 +10,25 @@ const {expect} = chai;
 
 export default ({}) => {
 	const answer = useSelector(R.path(["app", "lesson1", "numbers"]));
+	let syntaxError = false;
+
+	try {
+		if (answer) eval(ts2es6(R.flatten(test1(answer || "()")).join("\n")).replace(/const /g, "var "));
+	} catch(ex) {
+		syntaxError = true;
+	}
 
 	useEffect(() => {
 		document.getElementById("exercise-1-tests").innerHTML = "";
 
 		setTimeout(() => {
 			describe("Lesson 1 Exercise Tests", () => {
-				eval(ts2es6(R.flatten(test1(answer || "()")).join("\n")).replace(/const /g, "var "));
+				const run = (!answer || syntaxError) ? xit : it;
 
-				it('"numbers" is an array', () => expect(Array.isArray(numbers)).eql(true));
-				it('"numbers" has more than one element', () => expect(numbers.length > 1).eql(true));
-				it('all elements of "numbers" are numbers', () => expect(numbers.every(number => typeof(number) === "number")));
-				it('sum of elements in "numbers" equals 20', () => expect(numbers.reduce((a, b) => a + b)).eql(20));
+				run('"numbers" is an array', () => expect(Array.isArray(numbers)).eql(true));
+				run('"numbers" has more than one element', () => expect(numbers.length > 1).eql(true));
+				run('all elements of "numbers" are numbers', () => expect(numbers.every(number => typeof(number) === "number")));
+				run('sum of elements in "numbers" equals 20', () => expect(numbers.reduce((a, b) => a + b)).eql(20));
 			});
 
 			mocha.setup({
@@ -34,6 +41,7 @@ export default ({}) => {
 	return 	<div className="code-block exercises">
 		<li>Define an array <b>numbers</b> that contains multiple numbers, and only numbers, that add up to 20.</li>
 		{getReconstructedHtml(test1(<TextEdit path={["lesson1", "numbers"]}/>))}
+		{syntaxError ? <div className="mocha"><h2 className="test fail">Syntax Error</h2></div> : null}
 		<div id="exercise-1-tests" className="mocha"></div>
 	</div>;
 };
