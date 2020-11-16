03021af2a371d1b6c740071c4f8d3dd74a5cc03d
Darryl Morris <tacitscript@users.noreply.github.com>
2020-11-16 22:32:59 +0000

    add rounding

--- test/logic/main.js
+++ test/logic/main.js
@@ -8,7 +8,7 @@ describe("Problems", () => {
 	describe("o", () => {
 	/*ts
 		avg			.(+$ #)./$
-		solution	_1%._.+$.=0%.1%.].(1%.].avg)@
+		solution	_1%._.+$.=0%.1%.].(1%.].avg.{"Math.round")@
 	*/
 		it("solution([2, 3, 7, 0, 20, 10, 0, 1, 0]) eql [4, 15, 1]", () => expect(solution([2, 3, 7, 0, 20, 10, 0, 1, 0])).eql([4, 15, 1]));
 	});
