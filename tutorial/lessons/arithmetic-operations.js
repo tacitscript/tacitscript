8a3abe4cac7d76592cc12630ef0440d0d3b75df2
Darryl Morris <tacitscript@users.noreply.github.com>
2020-11-24 23:30:09 +0000

    text updates

--- tutorial/lessons/arithmetic-operations.js
+++ tutorial/lessons/arithmetic-operations.js
@@ -4,14 +4,14 @@ export default {
 	id: "a",
 	name: "Arithmetic Operations",
 	description: <div>
-		<p>tacitscript uses the standard binary arithmetic operators <a href="#add">+ (add)</a>, <a href="#subtract">- (subtract)</a>, <a href="#multiply">* (multiply)</a> and <a href="#divide">/ (divide)</a>.</p>
-		<p>Except for the operators <a href="#dot">.</a> and <a href="#comma">,</a> all tacitscript operators have equal precedence, and are strictly <i>left-associative</i>.</p>
+		<p>tacitscript uses the standard binary arithmetic operations <a href="#add">+ (add)</a>, <a href="#subtract">- (subtract)</a>, <a href="#multiply">* (multiply)</a> and <a href="#divide">/ (divide)</a>.</p>
+		<p>Except for the operators <a href="#dot">dot (.)</a> and <a href="#comma">comma (,)</a> all tacitscript operators have equal precedence, and are strictly <i>left-associative</i>.</p>
 		<div className="code-block">2+3*4</div>
 		<p>is intepreted as</p>
 		<div className="code-block">(2+3)*4</div>
 		<p>Parentheses are used to override the default evaluation order.</p>
 		<div className="code-block">2+(3*4)</div>
-		<p>Note that spaces cannot be used to separate terms in tacitscript (their meaning will be detailed shortly.)</p>
+		<p>Note that spaces cannot be used to separate terms in tacitscript (their meaning will be detailed below.)</p>
 	</div>,
 	exercise: {
 		question: "Define a tacitscript expression that:",
