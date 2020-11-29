65d59cbeddbf2de17a055e8da3ac9262999e256a
Darryl Morris <tacitscript@users.noreply.github.com>
2020-11-29 18:58:52 +0000

    operator chaining complete

--- tutorial/lessons/operator-chaining.js
+++ tutorial/lessons/operator-chaining.js
@@ -1,38 +1,40 @@
 import TextEdit from "../components/text-edit.js";
+import toDecimalPlaces from "../../common/src/to-decimal-places.js";
+
+const toCelsius = value => (value - 32) / 9 * 5;
 
 export default {
-	id: "e",
+	id: "f",
 	name: "Operator Chaining",
 	description: <div>
 		<p>The <a href="#pipe">. (pipe)</a> operation takes a unary operator to the left and right. The resulting operator passes a value through the left operator, then feeds the result through the right operator.</p>
 		<div className="code-block">{`halfAddOne\t/2.+1\ncalculation\thalfAddOne4\t\t\t\t\tequals 3`}</div>
 		<p>The <a href="#dot">dot (.)</a> and <a href="#comma">comma (,)</a> operators have lowest precedence, so parentheses are not required around <span className="code">+1</span> above.</p>
+		<p>The related <a href="#binary-unary-pipe">. (binaryUnaryPipe)</a> operation takes a binary operator on the left.</p>
+		<div className="code-block">{`average\t\t+./2\ncalculation\t5average7\t\t\t\t\tequals 6`}</div>
 	</div>,
 	exercise: {
-		question: <span>Define the operator <b>inverse</b> such that:</span>,
+		question: <div>
+			<div>To convert from Farenheit to Celsius, we subtract 32, divide by 9, then multiply by 5.</div>
+			<div>Define the operator <b>toCelsius</b> such that:</div>
+		</div>,
 		getJs: def => `const solution = /*ts ${def} */;`,
 		getHtml: details => <div className="single-line name-expression">
-			<div className="name">inverse</div>
+			<div className="name">toCelsius</div>
 			<TextEdit {...{...details, multiline: true}}/>
 		</div>,
+		getTestValue: () => Math.floor(Math.random() * 99) + 1,
 		tests: [
 			{description: "it is an operator", condition: ({solution}) => typeof solution === "function"},
-			{description: "it returns the reciprocal of any number passed to it", condition: ({solution}) => {
-				const testValue = Math.random();
-
+			{description: testValue => <span><b>toCelsius</b>{`${testValue} equals ${toDecimalPlaces(toCelsius(testValue), 4)}`}</span>, condition: ({solution, testValue}) => {
 				if (typeof solution !== "function") return false;
 
-				return Math.abs(solution(testValue) - (1 / testValue)) < 1E-10;
-			}},
-			{description: <span><b>inverse</b>0 equals <i>undefined</i></span>, condition: ({solution}) => {
-				if (typeof solution !== "function") return false;
-
-				return solution(0) == undefined;
+				try {
+					return Math.abs(solution(testValue) - toCelsius(testValue)) < 1E-10;
+				} catch (ex) {
+					return false;
+				}
 			}},
 		],
 	},
-	epilogue: <div>
-		<p><i>undefined</i> is a special value that has no representation within tacitscript (you can't use it in an expression.)<br/>It is a <i>toxic value</i> in that any calculation involving <i>undefined</i> will also equal <i>undefined</i>.</p>
-		<div className="code-block"><span>{`calculation\t1/0+2\t\t\t\t\t\tequals `}<i>undefined</i></span></div>
-	</div>,
 };
