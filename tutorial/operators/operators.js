950c8a1d6fa0dfc3c4e368e803247b4b41443770
Darryl Morris <tacitscript@users.noreply.github.com>
2020-12-02 19:53:17 +0000

    underscore entry

--- tutorial/operators/operators.js
+++ tutorial/operators/operators.js
@@ -2,10 +2,12 @@ import plus from "./plus.js";
 import minus from "./minus.js";
 import asterisk from "./asterisk.js";
 import slash from "./slash.js";
+import underscore from "./underscore.js";
 
 export default [
 	asterisk,
 	minus,
 	plus,
 	slash,
+	underscore,
 ];
---
+++ tutorial/operators/underscore.js
@@ -0,0 +1,11 @@
+
+export default {
+	id: "underscore",
+	symbol: "_",
+	type: "Unary",
+	sections: [{
+		id: "negative",
+		type: "NN",
+		examples: "negative5\t_5\t\t\t\t\tequals -5",
+	}],
+};
