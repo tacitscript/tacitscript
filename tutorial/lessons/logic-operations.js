4e73461b349144383276a1edc6abb46a35d2d232
Darryl Morris <tacitscript@users.noreply.github.com>
2020-12-07 23:44:22 +0000

    logic operators text

--- tutorial/lessons/logic-operations.js
+++ tutorial/lessons/logic-operations.js
@@ -3,19 +3,18 @@ export default {
 	id: "i",
 	name: "Logic Operations",
 	description: <div>
-		<p>The <a href="#andValue">& (andValue)</a> operation returns the right argument if the left argument is not false.</p>
+		<p>The <a href="#andValue">& (andValue)</a> operation returns the right argument if the left argument is not <i>false</i>. Otherwise, it returns <i>false</i>.</p>
 		<div className="code-block"><span>check           ""&amp;1                                            equals 1 (0 and "" are both <i>truthy</i>)</span></div>
 		<p>The <a href="#andPredicate">& (andPredicate)</a> operation checks that a value conforms to two conditions.</p>
 		<div className="code-block"><span>doubleCheck     &gt;2&amp;(&lt;6)                                         doubleCheck7=()</span></div>
-		<p><a href="#lessThan">&lt; (lessThan)</a> supports number comparison, and string comparison by <i>dictionary-order</i>.</p>
+		<p><a href="#bar">bar (|)</a> is the only operator that may return a defined value should either of its arguments be <i>undefined</i>.</p>
+		<p>The <a href="#orValue">| (orValue)</a> operation returns the left argument, unless it is <i>false</i> or <i>undefined</i>. In this case, it returns the right.</p>
 		<div className="code-block">
-			<span>numberLess      2&lt;3                                             is <i>true</i></span>
-			<span>stringLess      "cad"&lt;"bad"                                     is <i>false</i></span>
+			<span>falseOrTwo      ()|2                                            equals 2</span>
 		</div>
-		<p><a href="#greaterThan">&gt; (greaterThan)</a></p>
+		<p>The <a href="#orPredicate">| (orPredicate)</a> tests a value against the left condition; then on fail, tests against the right.</p>
 		<div className="code-block">
-			<span>numberGreater   2&gt;3                                             is <i>false</i></span>
-			<span>stringGreater   "cad"&gt;"bad"                                     is <i>true</i></span>
+			<span>positiveOrEven  &gt;0|(%2.=0)                                      positiveOrEven(_2) is <i>true</i></span>
 		</div>
 	</div>,
 };
