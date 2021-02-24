670cecae86974fab677256db4f6303620b54ca18
Darryl Morris <tacitscript@users.noreply.github.com>
2021-02-24 15:40:02 +0000

    add difference

--- test/logic/lodash.js
+++ test/logic/lodash.js
@@ -2,12 +2,20 @@ const {expect} = chai;
 import ts from "tacitscript";
 
 export default () => {
-	describe("chunk", () => {
+	describe("difference", () => {
 	/*ts
-		chunk			:,(.(; ) ;).%$
-		result			3chunk("a" "b" "c" "d")
+		difference		:,(@,~.! ;).*$
+		result			(2 3)difference(2 1)
 	*/
-		it("solved", () => expect(result).eql([["a", "b", "c"], ["d"]]));
+		it("solved", () => expect(result).eql([1]));
+	});
+
+	describe("concat", () => {
+	/*ts
+		concat			((}.="A" ;) .(; ))?@.+$
+		result			concat((1 ) 2 (3 ) ((4 ) ))
+	*/
+		it("solved", () => expect(result).eql([1, 2, 3, [4]]));
 	});
 
 	describe("compact", () => {
@@ -18,11 +26,13 @@ export default () => {
 		it("solved", () => expect(result).eql([0, 1, 2, 3]));
 	});
 
-	describe("concat", () => {
+	describe("chunk", () => {
 	/*ts
-		concat			((}.="A" ;) .(; ))?@.+$
-		result			concat((1 ) 2 (3 ) ((4 ) ))
+		chunk			:,(.(; ) ;).%$
+		result			3chunk("a" "b" "c" "d")
+		result2			2chunk("a" "b" "c" "d")
 	*/
-		it("solved", () => expect(result).eql([1, 2, 3, [4]]));
+		it("solved", () => expect(result).eql([["a", "b", "c"], ["d"]]));
+		it("solved2", () => expect(result2).eql([["a", "b"], ["c", "d"]]));
 	});
 };
