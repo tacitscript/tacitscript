ff58c327e10e51560e2c7ceaa4b21e6db6540626
Darryl Morris <tacitscript@users.noreply.github.com>
2021-06-16 17:41:01 +0100

    text updates

--- tutorial/lessons/operator-overloading.js
+++ tutorial/lessons/operator-overloading.js
@@ -4,13 +4,14 @@ export default {
 	id: "c",
 	name: "Operator Overloading",
 	description: <div>
-		<p>The <a href="#plus">plus (+)</a> operator can be used for <a href="#concat">(+) concat</a> operations over strings and arrays.</p>
+		<p>The particular operation that an operator resolves to, in an expression, depends on the types of the surrounding symbols.</p>
+		<p>Between strings or arrays, the <a href="#plus">plus (+)</a> operator will resolve to a <a href="#concat">(+) concat</a> operation.</p>
 		<div className="code-block">{`(1 2 3)+(4 5 6)\n"Hello, "+"World"`}</div>
 		<p>The operations of a particular operator will be related in meaning, and either all <b>binary</b> (taking arguments to the left and right), or all <b>unary</b> (taking one argument to the right.)<br/>
 		To denote negative numbers, we use the <a href="#negative">_ (negative)</a> operation of the unary <a href="#underscore">underscore (_)</a> operator.</p>
 		<div className="code-block">_5.5</div>
 		<p>Many operators take other operators as arguments. With strict left-associativity, <span className="code">2+_1</span> becomes a syntax error, as the two operators have equal precedence and there is no operation that 
-		supports <a href="#plus">plus (+)</a> to the left of <a href="#underscore">underscore (_)</a>. Parentheses are required in this case.</p>
+		supports <span className="code">2+</span> to the left of <a href="#underscore">underscore (_)</a>. Parentheses are required in this case.</p>
 		<div className="code-block">2+(_1)</div>
 		<p>The operation that resolves will depend on the types of the arguments used. <a href="#add">(+) add</a>, and <a href="#concat">(+) concat</a> over strings, act as type-conversion operations by supporting any 
 		non-operator type to the right.</p>
