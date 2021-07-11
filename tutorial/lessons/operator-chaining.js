620066530f9588af919c7ec3c45797d9ca08cb17
Darryl Morris <tacitscript@users.noreply.github.com>
2021-07-11 23:44:27 +0100

    update to ftoc

--- tutorial/lessons/operator-chaining.js
+++ tutorial/lessons/operator-chaining.js
@@ -1,7 +1,7 @@
 import TextEdit from "../components/text-edit.js";
 import toDecimalPlaces from "../../common/src/to-decimal-places.js";
 
-const toCelsius = value => (value - 32) / 9 * 5;
+const fToC = value => (value - 32) / 9 * 5;
 
 export default {
 	id: "f",
@@ -14,21 +14,21 @@ export default {
 	exercise: {
 		question: <div>
 			<div>To convert from Farenheit to Celsius, we subtract 32, divide by 9, then multiply by 5.</div>
-			<div>Define the operator <b>toCelsius</b> such that:</div>
+			<div>Define the operator <b>fToC</b> such that:</div>
 		</div>,
 		getJs: def => `const solution = /*ts ${def} */;`,
 		getHtml: details => <div className="single-line name-expression">
-			<div className="name">toCelsius</div>
+			<div className="name">fToC</div>
 			<TextEdit {...{...details, multiline: true, solution: "-32./9.*5"}}/>
 		</div>,
 		getTestValue: () => Math.floor(Math.random() * 99) + 1,
 		tests: [
 			{description: "it is an operator", condition: ({solution}) => typeof solution === "function"},
-			{description: testValue => <span><b>toCelsius</b>{`${testValue} equals ${toDecimalPlaces(toCelsius(testValue), 4)}`}</span>, condition: ({solution, testValue}) => {
+			{description: testValue => <span><b>fToC</b>{`${testValue} equals ${toDecimalPlaces(fToC(testValue), 4)}`}</span>, condition: ({solution, testValue}) => {
 				if (typeof solution !== "function") return false;
 
 				try {
-					return Math.abs(solution(testValue) - toCelsius(testValue)) < 1E-10;
+					return Math.abs(solution(testValue) - fToC(testValue)) < 1E-10;
 				} catch (ex) {
 					return false;
 				}
