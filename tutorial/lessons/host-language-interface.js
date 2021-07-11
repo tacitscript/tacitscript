962287803c0a27daee9a2d89712aeb53193a289e
Darryl Morris <tacitscript@users.noreply.github.com>
2021-07-11 21:31:27 +0100

    add tests for host language exercise

--- tutorial/lessons/host-language-interface.js
+++ tutorial/lessons/host-language-interface.js
@@ -1,5 +1,11 @@
 import TextEdit from "../components/text-edit.js";
 
+const {css} = Glamor;
+
+const labelStyle = css({
+	margin: "0 2.5rem 0 1.9rem",
+});
+
 export default {
 	id: "host-language-interface",
 	name: "Host Language Interface",
@@ -8,36 +14,34 @@ export default {
 		<p>tacitscript expressions can freely reference symbols from the current environment. tacitscript blocks implicity export defined symbols.</p>
 		<div className="code-block">{`const pi = Math.PI;
 const degressToRadians = /*ts *(pi/180) */;
-
 const arcLength = (r, theta) => r * theta;
 /*ts
     unitArcLength	1arcLength\r
 */
-
 const circumference = unitArcLength(degreesToRadians(360)); // equals 2pi`}</div>
 		<p>Note that binary operators are imported and exported as functions of two arguments.</p>
 	</div>,
 	exercise: {
 		question: <span>Define the operator/function <b>marathonSpeed</b> such that:</span>,
-		getJs: def => `const solution = /*ts ${def} */;`,
+		getJs: def => `
+const marathonMiles = 26.2188;
+const speed = (distance, time) => distance / time;
+/*ts
+		marathonSpeed ${def}
+*/
+const solution = marathonSpeed(2.0275);`,
 		getHtml: details => <div><br/><div className="rule"/><span>{`
 const marathonMiles = 26.2188;
 const speed = (distance, time) => distance / time;
-
 /*ts`}</span><span className="single-line name-expression">
-		<span className="name">marathonSpeed</span>
+		<span className="name" {...labelStyle}>marathonSpeed</span>
 		<TextEdit {...{...details, solution: "marathonMiles,speed"}}/>
 	</span><span>{`*/
-
-const recordAverageMph = marathonSpeed(2.0275);`}</span></div>,
+const recordMph = marathonSpeed(2.0275);`}</span></div>,
 		getTestValue: () => Math.floor(Math.random() * 99) + 1,
 		tests: [
-			{description: "it is an operator", condition: ({solution}) => typeof solution === "function"},
-			{description: <span><b>inverse</b>0 is <i>undefined</i></span>, condition: ({solution}) => {
-				if (typeof solution !== "function") return false;
-
-				return solution(0) == undefined;
-			}},
+			{description: <span>it uses <i>marathonMiles</i> and <i>speed</i></span>, condition: ({def}) => ["marathonMiles", "speed"].every(token => def.includes(token))},
+			{description: "it calculates the record average marathon speed (12.9mph)", condition: ({solution}) => (solution - 12.93159) < 1E-5},
 		],
 	},
 	epilogue: <div>
