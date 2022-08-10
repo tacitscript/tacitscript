8c415745b569853cad3894c80b347c01f7a87a68
Darryl <dwsmorris@hotmail.com>
2022-08-10 15:35:15 +0100

    rewording

--- tutorial/lessons/operator-chaining.js
+++ tutorial/lessons/operator-chaining.js
@@ -9,7 +9,7 @@ export default {
 	description: <div>
 		<p>The <a href="#pipe">(.) pipe</a> operation takes a unary operator to the left and right. The resulting operator passes a value through the left operator, then feeds the result through the right operator.</p>
 		<div className="code-block">{`halfAddOne\t/2.+1\ncalculation\thalfAddOne4\t\t\t\t\tequals 3`}</div>
-		<p>The <a href="#dot">dot (.)</a> and <a href="#comma">comma (,)</a> operators have lowest precedence, so parentheses are not required around <span className="code">+1</span> above.</p>
+		<p>The <a href="#dot">dot (.)</a> and <a href="#comma">comma (,)</a> operators break <i>left-associativity</i> and have lowest precedence (the only exceptions to this rule.) Therefore, parentheses are not required around <span className="code">+1</span> above.</p>
 	</div>,
 	exercise: {
 		question: <div>
