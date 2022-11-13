e52e73f21f5228f82f4d660ad7c5c5f13bed1182
Darryl <dwsmorris@hotmail.com>
2022-11-13 20:13:54 +0000

    complete fizzbuzz

--- tutorial/lessons/fizzbuzz.js
+++ tutorial/lessons/fizzbuzz.js
@@ -26,7 +26,7 @@ export default {
 			<p><i>Fizzbuzz</i> is a popular interview question.</p>
 			<p>Define the expression <b>fizzbuzz</b> that generates:</p>
 		</div>,
-		getJs: def => `const solution = /*ts ${def},""+ */;`,
+		getJs: def => `const solution = /*ts (${def} ${def},""+) */;`,
 		getHtml: details => <div {...style}>
 			<div className="single-line name-expression">
 				<div className="name">fizzbuzz</div>
@@ -38,44 +38,58 @@ export default {
 			<div className="console-title">CONSOLE</div>
 			<div className="output">
 				<span className="prompt">&gt;</span>
-				<span>{details.solution}</span>
+				<span>{R.pathOr("ERROR", ["solution", 1], details)}</span>
 			</div>
 		</div>,
 		getTestValue: index => `${(Math.floor(Math.random() * 1000) * (index ? 1 : -1)) / 10}`.replace(/-/, "_"),
 		hint1: "Use operators: & @ ` , . = ^ % + ? ;",
 		hint2: "generate 1 to 100, map over array replacing values appropriately",
 		tests: [
-			{description: `an array of length 100`, condition: ({solution, testValue}) => {
+			{description: `an array containing integers 1 to 100 (inclusive), except`, condition: ({solution, testValue}) => {
 				try {
-					return Math.abs(+solution(testValue).replace(/_/, "-") - testValue.replace(/_/, "-") - 1) < 1E-10;
+					return R.addIndex(R.all)((value, i) => {
+						const index = i + 1;
+
+						if (((index % 3) !== 0) && ((index % 5) !== 0)) return value === index;
+						return true;
+					}, solution[0]);
 				} catch (e) {
 					return false;
 				}
 			}},
-			{description: `where every third element has "Fizz"`, condition: ({solution, testValue}) => {
+			{description: `for multiples of three, show "Fizz"`, condition: ({solution, testValue}) => {
 				try {
-					return Math.abs(+solution(testValue) - testValue - 1) < 1E-10;
+					return R.addIndex(R.all)((value, i) => {
+						const index = i + 1;
+
+						if (((index % 3) === 0) && ((index % 5) !== 0)) return value === "Fizz";
+						return true;
+					}, solution[0]);
 				} catch (e) {
 					return false;
 				}
 			}},
-			{description: `where every fifth element has "Buzz"`, condition: ({solution, testValue}) => {
+
+			{description: `for multiples of five, show "Buzz"`, condition: ({solution, testValue}) => {
 				try {
-					return Math.abs(+solution(testValue) - testValue - 1) < 1E-10;
+					return R.addIndex(R.all)((value, i) => {
+						const index = i + 1;
+
+						if (((index % 3) !== 0) && ((index % 5) === 0)) return value === "Buzz";
+						return true;
+					}, solution[0]);
 				} catch (e) {
 					return false;
 				}
 			}},
-			{description: `where elements at a multiple of both three and five have "FizzBuzz"`, condition: ({solution, testValue}) => {
+			{description: `for multiples of both three and five, show "FizzBuzz"`, condition: ({solution, testValue}) => {
 				try {
-					return Math.abs(+solution(testValue) - testValue - 1) < 1E-10;
-				} catch (e) {
-					return false;
-				}
-			}},
-			{description: `where remaining elements have the element index (starting with 1)`, condition: ({solution, testValue}) => {
-				try {
-					return Math.abs(+solution(testValue) - testValue - 1) < 1E-10;
+					return R.addIndex(R.all)((value, i) => {
+						const index = i + 1;
+
+						if (((index % 3) === 0) && ((index % 5) === 0)) return value === "FizzBuzz";
+						return true;
+					}, solution[0]);
 				} catch (e) {
 					return false;
 				}
