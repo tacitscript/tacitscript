aeee4f547df27e3139bee06068a74d8e00b52cf8
Darryl <dwsmorris@hotmail.com>
2022-11-13 00:05:33 +0000

    update at

--- tutorial/operators/apostrophe.js
+++ tutorial/operators/apostrophe.js
@@ -1,3 +1,4 @@
+import getOperationExamples from "../logic/get-operation-examples.js";
 
 export default {
 	id: "apostrophe",
@@ -6,16 +7,16 @@ export default {
 	sections: [{
 		id: "at",
 		type: "NA?\tNSS",
-		examples: <React.Fragment>
-			<span>{`elemArray\t1'(5 6 7)\t\t\t\t\tequals 6\nelemEArray\t0'( )\t\t\t\t\t\tis `}
-			<i>undefined</i></span>
-			<span>{`elemOOBArray\t3'(5 6 7)\t\t\t\t\tis `}
-			<i>undefined</i></span>
-			<span>{`elemNegArray\t_1'(5 6 7)\t\t\t\t\tequals 7\n\nelemString\t1'"Hello, World!"\t\t\t\tequals "e"\nelemEString\t0'""\t\t\t\t\t\tis `}
-			<i>undefined</i></span>
-			<span>{`elemOOBString\t13'"Hello, World!\t\t\t\tis `}
-			<i>undefined</i></span>
-			<span>{`elemNegString\t_1'"Hello, World!"\t\t\t\tequals "!"`}</span>
-		</React.Fragment>,
+		examples: getOperationExamples([
+			["elemArray", "1'(5 6 7)", "equals 6"],
+			["elemEmptyArray", `0'( )`, <span>is <i>undefined</i></span>],
+			["elemOOBArray", "3'(5 6 7)", <span>is <i>undefined</i></span>],
+			["negElemArray", `_1'(5 6 7)`, "equals 7 (wrap around)"],
+			[<br/>],
+			["elemString", `1'"Hello, World!"`, `equals "e"`],
+			["elemEmptyString", `0'""`, <span>is <i>undefined</i></span>],
+			["elemOOBString", `13'"Hello, World!`, <span>is <i>undefined</i></span>],
+			["negElemString", `_1'"Hello, World!"`, `equals "!" (wrap around)`],
+		]),
 	}],
 };
