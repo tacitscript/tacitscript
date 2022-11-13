7644aa234dde748ba7b36bf65fd28f3cda50e192
Darryl <dwsmorris@hotmail.com>
2022-11-13 00:10:39 +0000

    dont duplicate equals

--- tutorial/lessons/comparisons.js
+++ tutorial/lessons/comparisons.js
@@ -5,10 +5,6 @@ export default {
 	name: "Comparisons",
 	description: <div>
 		<p>Comparison operators return a boolean.</p>
-		<p><a href="#equality">= (equality)</a> makes a deep <i>by-value</i> comparison.</p>
-		<div className="code-block">{getOperationExamples([
-			["deepCheck", '("abc" (1 2 3))=("abc" (1 2 4))', <span>is <i>false</i></span>],
-		])}</div>
 		<p><a href="#lessThan">&lt; (lessThan)</a> supports number comparison, and string comparison by <i>dictionary-order</i>.</p>
 		<div className="code-block">{getOperationExamples([
 			["numberLess", "2<3", <span>is <i>true</i></span>],
--- tutorial/operators/equals.js
+++ tutorial/operators/equals.js
@@ -8,7 +8,7 @@ export default {
 		id: "equality",
 		type: "VVB",
 		examples: getOperationExamples([
-			["deepCheck", `("abc" (1 2 3))=("abc" (1 2 3))`, <span>is <i>true</i> (deep <i>by-value</i> comparison)</span>],
+			["deepCheck", `("abc" (1 2 3))=("abc" (1 2 4))`, <span>is <i>false</i> (deep <i>by-value</i> comparison)</span>],
 			["mixedType", '"string"=4', <span>is <i>false</i></span>],
 			["includeOperators", '(2* 1)=(2* 1)', <span>is <i>undefined</i> (cannot compare operators)</span>]
 		]),
