93e48963a9d687a8ceca734d58cb97ee568649c6
Darryl <dwsmorris@hotmail.com>
2022-11-22 23:22:08 +0000

    hypotenuse problem

--- test/logic/sicp.js
+++ test/logic/sicp.js
@@ -9,5 +9,12 @@ export default () => {
             */
             it("3sumOfSquares4=25", () => expect(sumOfSquares(3, 4)).eql(25));
         });
+
+        describe("hypotenuse :.^2@.+$.^0.5", () => {
+            /*ts
+                hypotenuse  :.^2@.+$.^0.5
+            */
+            it("3hypotenuse4=5", () => expect(hypotenuse(3, 4)).eql(5));
+        });
     });
 };
