9d1453adf066d2c56969ec2828855f47c951cc50
Darryl <dwsmorris@hotmail.com>
2022-11-22 23:19:25 +0000

    start sicp work

--- test/logic/main.js
+++ test/logic/main.js
@@ -1,21 +1,23 @@
 const {expect} = chai;
-import ts from "tacitscript";
-import problems from "./problems.js";
-import lodash from "./lodash.js";
-import miscellaneous from "./miscellaneous.js";
-import operators from "./operators.js";
-import rosettaCode from "./rosetta-code.js";
-import underscore from "./underscore.js";
-import haskell from "./haskell.js";
+// import ts from "tacitscript";
+// import problems from "./problems.js";
+// import lodash from "./lodash.js";
+// import miscellaneous from "./miscellaneous.js";
+// import operators from "./operators.js";
+// import rosettaCode from "./rosetta-code.js";
+// import underscore from "./underscore.js";
+// import haskell from "./haskell.js";
+import sicp from "./sicp.js";
 
 mocha.setup('bdd');
 
-haskell();
-rosettaCode();
-problems();
-underscore();
-lodash();
-operators();
-miscellaneous();
+sicp();
+// haskell();
+// rosettaCode();
+// problems();
+// underscore();
+// lodash();
+// operators();
+// miscellaneous();
 
 mocha.run();
---
+++ test/logic/sicp.js
@@ -0,0 +1,13 @@
+const {expect} = chai;
+import ts from "tacitscript";
+
+export default () => {
+    describe("SICP", () => {
+        describe("1.1.1 sumOfSquares :.^2@.+$", () => {
+            /*ts
+                sumOfSquares    :.^2@.+$
+            */
+            it("3sumOfSquares4=25", () => expect(sumOfSquares(3, 4)).eql(25));
+        });
+    });
+};
