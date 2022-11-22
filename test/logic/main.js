04192921fe4b5247028e5f92345d98538e150ed7
Darryl <dwsmorris@hotmail.com>
2022-11-22 23:37:45 +0000

    complete chp 1 sicp

--- test/logic/main.js
+++ test/logic/main.js
@@ -1,23 +1,23 @@
 const {expect} = chai;
-// import ts from "tacitscript";
-// import problems from "./problems.js";
-// import lodash from "./lodash.js";
-// import miscellaneous from "./miscellaneous.js";
-// import operators from "./operators.js";
-// import rosettaCode from "./rosetta-code.js";
-// import underscore from "./underscore.js";
-// import haskell from "./haskell.js";
+import ts from "tacitscript";
+import problems from "./problems.js";
+import lodash from "./lodash.js";
+import miscellaneous from "./miscellaneous.js";
+import operators from "./operators.js";
+import rosettaCode from "./rosetta-code.js";
+import underscore from "./underscore.js";
+import haskell from "./haskell.js";
 import sicp from "./sicp.js";
 
 mocha.setup('bdd');
 
 sicp();
-// haskell();
-// rosettaCode();
-// problems();
-// underscore();
-// lodash();
-// operators();
-// miscellaneous();
+haskell();
+rosettaCode();
+problems();
+underscore();
+lodash();
+operators();
+miscellaneous();
 
 mocha.run();
--- test/logic/sicp.js
+++ test/logic/sicp.js
@@ -2,19 +2,26 @@ const {expect} = chai;
 import ts from "tacitscript";
 
 export default () => {
-    describe("SICP", () => {
-        describe("1.1.1 sumOfSquares :.^2@.+$", () => {
-            /*ts
-                sumOfSquares    :.^2@.+$
-            */
-            it("3sumOfSquares4=25", () => expect(sumOfSquares(3, 4)).eql(25));
-        });
+	describe("SICP", () => {
+		describe("1.1.1 sumOfSquares :.^2@.+$", () => {
+			/*ts
+				sumOfSquares    :.^2@.+$
+			*/
+			it("3sumOfSquares4=25", () => expect(sumOfSquares(3, 4)).eql(25));
+		});
 
-        describe("hypotenuse :.^2@.+$.^0.5", () => {
-            /*ts
-                hypotenuse  :.^2@.+$.^0.5
-            */
-            it("3hypotenuse4=5", () => expect(hypotenuse(3, 4)).eql(5));
-        });
-    });
+		describe("hypotenuse :.^2@.+$.^0.5", () => {
+			/*ts
+				hypotenuse  :.^2@.+$.^0.5
+			*/
+			it("3hypotenuse4=5", () => expect(hypotenuse(3, 4)).eql(5));
+		});
+
+		describe("1.3 sumOfTwoLarger ;<.1%.].^2@.+$", () => {
+			/*ts
+				sumOfTwoLarger	;<.1%.].^2@.+$
+			*/
+			it(";<.1%.].^2@.+$(4 2 3)=25", () => expect(sumOfTwoLarger([4, 2, 3])).eql(25));
+		});
+	});
 };
