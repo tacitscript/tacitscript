cb0d0dae308c3961f436268d220633814439c5df
Darryl <dwsmorris@hotmail.com>
2022-11-23 23:11:19 +0000

    update sicp tests

--- test/logic/sicp.js
+++ test/logic/sicp.js
@@ -23,5 +23,26 @@ export default () => {
 			*/
 			it(";<.1%.].^2@.+$(4 2 3)=25", () => expect(sumOfTwoLarger([4, 2, 3])).eql(25));
 		});
+
+		describe("recursive factorial ((=0 1`) .(; -1.fact).*$)?", () => {
+			/*ts
+				fact	((=0 1`) .(; -1.fact).*$)?
+			*/
+			it("((=0 1`) .(; -1.fact).*$)?4=24", () => expect(fact(4)).eql(24));
+		});
+
+		describe("generator factorial +1^.*$", () => {
+			/*ts
+				fact	+1^.*$
+			*/
+			it("+1^.*$4=24", () => expect(fact(4)).eql(24));
+		});
+
+		describe("fibonacci recursive ((=0 0`) (=1 1`) .(-1 -2).fib@.+$)?", () => {
+			/*ts
+				fib		((=0 0`) (=1 1`) .(-1 -2).fib@.+$)?
+			*/
+			it("((=0 0`) (=1 1`) .(-1 -2).fib@.+$)?4=3", () => expect(fib(4)).eql(3));
+		});
 	});
 };
