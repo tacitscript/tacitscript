c9e004aa59fe0f4909b60e1c284619a00f9eb4c4
Darryl <dwsmorris@hotmail.com>
2022-11-26 20:49:09 +0000

    reverse order of sicp tests

--- test/logic/sicp.js
+++ test/logic/sicp.js
@@ -3,81 +3,17 @@ import ts from "tacitscript";
 
 export default () => {
 	describe("SICP", () => {
-		describe("1.1.1 sumOfSquares :.^2@.+$", () => {
+		xdescribe("gcd", () => {
 			/*ts
-				sumOfSquares    :.^2@.+$
+				log			>({"console.log")
+				//recurse		.(] %$).gcd$
+				//gcd			:.((].=0 [) .(] %$).gcd$)?
+				//value		gcd$(15 5)
+				//add			:.+$
+				//val			:.add$
+				//solution	15gcd5
 			*/
-			it("3sumOfSquares4=25", () => expect(sumOfSquares(3, 4)).eql(25));
-		});
-
-		describe("hypotenuse :.^2@.+$.^0.5", () => {
-			/*ts
-				hypotenuse  :.^2@.+$.^0.5
-			*/
-			it("3hypotenuse4=5", () => expect(hypotenuse(3, 4)).eql(5));
-		});
-
-		describe("1.3 sumOfTwoLarger ;<.1%.].^2@.+$", () => {
-			/*ts
-				sumOfTwoLarger	;<.1%.].^2@.+$
-			*/
-			it("sumOfTwoLarger(4 2 3)=25", () => expect(sumOfTwoLarger([4, 2, 3])).eql(25));
-		});
-
-		describe("factRec ((=0 1`) .(; -1.factRec).*$)?", () => {
-			/*ts
-				factRec	((=0 1`) .(; -1.factRec).*$)?
-			*/
-			it("factRec4=24", () => expect(factRec(4)).eql(24));
-		});
-
-		describe("factGen +1^.*$", () => {
-			/*ts
-				factGen	+1^.*$
-			*/
-			it("factGen4=24", () => expect(factGen(4)).eql(24));
-		});
-
-		describe("fibRec ((=0 0`) (=1 1`) .(-1 -2).fibRec@.+$)?", () => {
-			/*ts
-				fibRec		((=0 0`) (=1 1`) .(-1 -2).fibRec@.+$)?
-			*/
-			it("fibRec4=3", () => expect(fibRec(4)).eql(3));
-		});
-
-		describe("fibGen .(.(>,(#.) (_2%.].+$)`) (1 1)`).^$.]", () => {
-			/*ts
-				fibGen		.(.(>,(#.) (_2%.].+$)`) (1 1)`).^$.]
-			*/
-			it("fibGen6=8", () => expect(fibGen(6)).eql(8));
-		});
-
-		describe("1.11 f ((<3 ;) .(-1.f -2.f.*2 -3.f.*3).+$)?", () => {
-			/*ts
-				f	((<3 ;) .(-1.f -2.f.*2 -3.f.*3).+$)?
-			*/
-			it("f4=11", () => expect(f(4)).eql(11));
-		});
-
-		describe("1.12 pascal .(.(>,(#.) (].((0 )+ +(0 )).~.+$@)`) ((1 ) )`).^$", () => {
-			/*ts
-				pascal		.(.(>,(#.) (].((0 )+ +(0 )).~.+$@)`) ((1 ) )`).^$
-			*/
-			it("pascal5=((1 ) (1 1) (1 2 1) (1 3 3 1) (1 4 6 4 1))", () => expect(pascal(5)).eql([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]));
-		});
-
-		describe("expRec :.((=0 1`) .([ .([ ].-1).^$).*$)?", () => {
-			/*ts
-				expRec		:.((=0 1`) .([ .([ ].-1).^$).*$)?
-			*/
-			it("2expRec3=8", () => expect(expRec(2, 3)).eql(8));
-		});
-
-		describe("expGen :,(` ;).^$.*$", () => {
-			/*ts
-				expGen		:,(` ;).^$.*$
-			*/
-			it("3expGen2=9", () => expect(expGen(3, 2)).eql(9));
+			it("15gcd20=5", () => expect(5).eql(5));
 		});
 
 		xdescribe("fastExp", () => {
@@ -90,6 +26,83 @@ export default () => {
 				fastExp		:.(ifNoNOne ifNEvenSFE fastExpRec)?
 			*/
 			it("2fastExp7=128", () => expect(fastExp(2, 7)).eql(128));
+		});
+
+		describe("expGen :,(` ;).^$.*$", () => {
+			/*ts
+				expGen		:,(` ;).^$.*$
+			*/
+			it("3expGen2=9", () => expect(expGen(3, 2)).eql(9));
+		});
+
+		describe("expRec :.((=0 1`) .([ .([ ].-1).^$).*$)?", () => {
+			/*ts
+				expRec		:.((=0 1`) .([ .([ ].-1).^$).*$)?
+			*/
+			it("2expRec3=8", () => expect(expRec(2, 3)).eql(8));
+		});
+
+		describe("1.12 pascal .(.(>,(#.) (].((0 )+ +(0 )).~.+$@)`) ((1 ) )`).^$", () => {
+			/*ts
+				pascal		.(.(>,(#.) (].((0 )+ +(0 )).~.+$@)`) ((1 ) )`).^$
+			*/
+			it("pascal5=((1 ) (1 1) (1 2 1) (1 3 3 1) (1 4 6 4 1))", () => expect(pascal(5)).eql([[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]));
+		});
+
+		describe("1.11 f ((<3 ;) .(-1.f -2.f.*2 -3.f.*3).+$)?", () => {
+			/*ts
+				f	((<3 ;) .(-1.f -2.f.*2 -3.f.*3).+$)?
+			*/
+			it("f4=11", () => expect(f(4)).eql(11));
+		});
+
+		describe("fibGen .(.(>,(#.) (_2%.].+$)`) (1 1)`).^$.]", () => {
+			/*ts
+				fibGen		.(.(>,(#.) (_2%.].+$)`) (1 1)`).^$.]
+			*/
+			it("fibGen6=8", () => expect(fibGen(6)).eql(8));
+		});
+
+		describe("fibRec ((=0 0`) (=1 1`) .(-1 -2).fibRec@.+$)?", () => {
+			/*ts
+				fibRec		((=0 0`) (=1 1`) .(-1 -2).fibRec@.+$)?
+			*/
+			it("fibRec4=3", () => expect(fibRec(4)).eql(3));
+		});
+
+		describe("factGen +1^.*$", () => {
+			/*ts
+				factGen	+1^.*$
+			*/
+			it("factGen4=24", () => expect(factGen(4)).eql(24));
+		});
+
+		describe("factRec ((=0 1`) .(; -1.factRec).*$)?", () => {
+			/*ts
+				factRec	((=0 1`) .(; -1.factRec).*$)?
+			*/
+			it("factRec4=24", () => expect(factRec(4)).eql(24));
+		});
+
+		describe("1.3 sumOfTwoLarger ;<.1%.].^2@.+$", () => {
+			/*ts
+				sumOfTwoLarger	;<.1%.].^2@.+$
+			*/
+			it("sumOfTwoLarger(4 2 3)=25", () => expect(sumOfTwoLarger([4, 2, 3])).eql(25));
+		});
+
+		describe("hypotenuse :.^2@.+$.^0.5", () => {
+			/*ts
+				hypotenuse  :.^2@.+$.^0.5
+			*/
+			it("3hypotenuse4=5", () => expect(hypotenuse(3, 4)).eql(5));
+		});
+
+		describe("1.1.1 sumOfSquares :.^2@.+$", () => {
+			/*ts
+				sumOfSquares    :.^2@.+$
+			*/
+			it("3sumOfSquares4=25", () => expect(sumOfSquares(3, 4)).eql(25));
 		});
 	});
 };
