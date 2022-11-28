9a23d8fca5d2298736611570f6613234a9a49bf5
Darryl <dwsmorris@hotmail.com>
2022-11-28 20:35:48 +0000

    sameParity, deepReverse and flatten

--- test/logic/sicp.js
+++ test/logic/sicp.js
@@ -13,6 +13,29 @@ export default () => {
 	});
 
 	describe("SICP", () => {
+		describe(`2.28 flatten ((}.="A" flatten) ;)?@.{`, () => {
+			/*ts
+				unnestSingle		{((1 ))
+				flatten				((}.="A" flatten) ;)?@.{
+			*/
+			it("flatten(1 2 (3 (4 5 (6 )) 7) (8 9))=(1 2 3 4 5 6 7 8 9)", () => expect(flatten([1, 2, [3, [4, 5, [6]], 7], [8, 9]])).eql([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+		});
+
+		describe(`2.27 deepReverse ((}.="A" deepReverse) ;)?@._`, () => {
+			/*ts
+				deepReverse 		((}.="A" deepReverse) ;)?@._
+			*/
+			it("deepReverse((1 2) (3 4))=((4 3) (2 1))", () => expect(deepReverse([[1, 2], [3, 4]])).eql([[4, 3], [2, 1]]));
+		});
+
+		describe("2.20 sameParity .([.%2.=.(%2.) ;).*$", () => {
+			/*ts
+				sameParity			.([.%2.=.(%2.) ;).*$
+			*/
+			it("sameParity(1 2 3 4 5 6 7)=(1 3 5 7)", () => expect(sameParity([1, 2, 3, 4, 5, 6, 7])).eql([1, 3, 5, 7]));
+			it("sameParity(2 3 4 5 6 7)=(2 4 6)", () => expect(sameParity([2, 3, 4, 5, 6, 7])).eql([2, 4, 6]));
+		});
+
 		describe("cons :.(,apply) car [,", () => {
 			/*ts
 				comma				,
