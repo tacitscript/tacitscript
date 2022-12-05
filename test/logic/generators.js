0802773f44e0ca2bf02561f10dcb6d392e98be73
Darryl <dwsmorris@hotmail.com>
2022-12-05 20:17:07 +0000

    move sumPrimes

--- test/logic/generators.js
+++ test/logic/generators.js
@@ -16,5 +16,22 @@ export default () => {
 			*/
 			it("", () => expect().eql());
 		});
+
+		describe("", () => {
+			/*ts
+				divides					:._.%$.=0
+				findDivisor				:.((.(].^2 [).>$ [) (_.divides$ ]) .([ ].+1).findDivisor$)?
+				smallestDivisor			:2.findDivisor$
+				isPrime					.(smallestDivisor ;).=$
+				range					:.([.+ _.-$).^$
+				sumPrimes				range.isPrime*.+$
+			*/
+			it("2sumPrimes11=17", () => expect(sumPrimes(2, 11)).eql(17));
+			it("2range11=(2 3 4 5 6 7 8 9 10)", () => expect(range(2, 11)).eql([2, 3, 4, 5, 6, 7, 8, 9, 10]));
+			it("isPrime8=()", () => expect(isPrime(8)).eql(false));
+			it("isPrime7=!()", () => expect(isPrime(7)).eql(true));
+			it("14divide2=!()", () => expect(divides(2, 14)).eql(true));
+			it("15divides2=()", () => expect(divides(2, 15)).eql(false));
+		});
 	});
 };
