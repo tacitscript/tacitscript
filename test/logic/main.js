19aeedb458528d10d130fb61a31a5800f0cee0af
Darryl <dwsmorris@hotmail.com>
2022-12-07 16:51:28 +0000

    remove utilities

--- test/logic/main.js
+++ test/logic/main.js
@@ -8,13 +8,11 @@ import underscore from "./underscore.js";
 import haskell from "./haskell.js";
 import sicp from "./sicp.js";
 import adventOfCode from "./advent-of-code.js";
-import utilities from "./utilities.js";
 import generators from "./generators.js";
 
 mocha.setup('bdd');
 
 generators();
-utilities();
 adventOfCode();
 sicp();
 haskell();
