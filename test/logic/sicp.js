ccae691c6029b980e000468f83c7eb8e612380dc
Darryl <dwsmorris@hotmail.com>
2022-12-10 10:45:53 +0000

    first expressions

--- test/logic/sicp.js
+++ test/logic/sicp.js
@@ -8,4 +8,21 @@ export default () => {
 		*/
 		it("", () => expect().eql());
 	});
+
+	describe("SICP", () => {
+		describe("1.1.1 Expressions", () => {
+			/*ts
+				a			137+349
+				b			10/5
+				c			1000-334
+				d			2.7+10
+				e			5*99
+			*/
+			it("137+349=486", () => expect(a).eql(486));
+			it("10/5=2", () => expect(b).eql(2));
+			it("1000-334=666", () => expect(c).eql(666));
+			it("2.7+10=12.7", () => expect(d).eql(12.7));
+			it("5*99=495", () => expect(e).eql(495));
+		});
+	});
 };
