e962e689650dba990df84d00411e0cf370893a53
Darryl <dwsmorris@hotmail.com>
2022-12-10 10:30:20 +0000

    remove v1 test data

--- test/logic/main.js
+++ test/logic/main.js
@@ -1,26 +1,9 @@
 const {expect} = chai;
-import problems from "./problems.js";
-import lodash from "./lodash.js";
-import miscellaneous from "./miscellaneous.js";
-import operators from "./operators.js";
-import rosettaCode from "./rosetta-code.js";
-import underscore from "./underscore.js";
-import haskell from "./haskell.js";
+
 import sicp from "./sicp.js";
-import adventOfCode from "./advent-of-code.js";
-import generators from "./generators.js";
 
 mocha.setup('bdd');
 
-generators();
-adventOfCode();
 sicp();
-haskell();
-rosettaCode();
-problems();
-underscore();
-lodash();
-operators();
-miscellaneous();
 
 mocha.run();
