25438fc6a928a3d1959cf32dd44bfd36db244848
Darryl <dwsmorris@hotmail.com>
2022-12-18 14:20:50 +0000

    Revert "turn recursive to binary operator"
    This reverts commit 1f4e1c23037fd0a0e9f1e5d46e8464bfedf980be.

--- common/src/parser.js
+++ common/src/parser.js
@@ -401,7 +401,7 @@ const processTsBlock = function(userDefinitions) {return function(ts) {
 						const processedSymbols = result.processedSymbols;
 
 						const solution = processSymbols(processedSymbols, undefined, updatedDefinitions);
-						const declaration =  isRecursive ? "var " + variable + " =" + definitionSeparator + "(x, y) => " + definition + "(x, y);" : "const " + variable + " =" + definitionSeparator + definition + ";";
+						const declaration =  isRecursive ? "var " + variable + " =" + definitionSeparator + "x => " + definition + "(x); " + variable + '.types = [[["V", "V"], "V"], ["V", "V"]];' : "const " + variable + " =" + definitionSeparator + definition + ";";
 
 						const noComment = symbol.includes("\n");
 						js += declaration + (noComment ? "" : " //");
