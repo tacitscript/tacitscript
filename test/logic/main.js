ba67143c79fdc818ea90e2191b2dfda7f8efe4d1
Darryl <dwsmorris@hotmail.com>
2022-12-27 10:41:27 +0000

    add first new sicp from compile time expansion

--- test/logic/main.js
+++ test/logic/main.js
@@ -1,13 +1,13 @@
 const {expect} = chai;
 
-// import sicp from "./sicp.js";
+import sicp from "./sicp.js";
 import operators from "./operators.js";
 import parser from "./parser.js";
 
 mocha.setup('bdd');
 
+sicp();
 operators();
 parser();
-// sicp();
 
 mocha.run();
