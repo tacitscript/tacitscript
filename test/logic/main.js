29aa1dc29b09de1f5795c04f1f30040366404144
Darryl <dwsmorris@hotmail.com>
2023-02-01 15:25:06 +0000

    add initial sicp

--- test/logic/main.js
+++ test/logic/main.js
@@ -1,10 +1,12 @@
 const {expect} = chai;
 
+import sicp from "./sicp.js";
 import operators from "./operators.js";
 import parser from "./parser.js";
 
 mocha.setup('bdd');
 
+sicp();
 operators();
 parser();
 
--- test/logic/sicp.js
+++ test/logic/sicp.js
@@ -1,11 +1,27 @@
-const {expect} = chai;
+const {expect, assert} = chai;
 import ts from "tacitscript";
 
 export default () => {
-	xdescribe("", () => {
-		/*ts
 
+	describe("1.3 sumOfTwoLarger ;<.1%.].^2@.+$", () => {
+		/*ts
+			sumOfTwoLarger	;<.1%.].^2@.+$
 		*/
-		it("", () => expect().eql());
+		it("sumOfTwoLarger(4 2 3)=25", () => expect(sumOfTwoLarger([4, 2, 3])).eql(25));
 	});
+
+	describe("hypotenuse :.^2@.+$.^0.5", () => {
+		/*ts
+			hypotenuse  :.^2@.+$.^0.5
+		*/
+		it("3hypotenuse4=5", () => expect(hypotenuse(3, 4)).eql(5));
+	});
+
+	describe("1.1.1 sumOfSquares :.^2@.+$", () => {
+		/*ts
+			sumOfSquares    :.^2@.+$
+		*/
+		it("3sumOfSquares4=25", () => expect(sumOfSquares(3, 4)).eql(25));
+	});
+
 };
