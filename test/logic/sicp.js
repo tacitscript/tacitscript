0cd497921a620a694cbc640e6173ade44c6f52e9
Darryl <dwsmorris@hotmail.com>
2023-02-02 22:23:15 +0000

    readd much of sicp

--- test/logic/sicp.js
+++ test/logic/sicp.js
@@ -1,85 +1,85 @@
-const {expect, assert} = chai;
+const { expect, assert } = chai;
 import ts from "tacitscript";
 
 export default () => {
-	describe('complex numbers - revealing module .(: ((].="real" [.[) (].="imag" [.]) (].="mag" [.^2@.+$.^0.5) (].="add" [.(: (~.+$@.complexASV)`).(.$)) 0/0`)?`).(.$)', () => {
-		// NB if we pass a module here eg, complexASV(1 2)"add"(complexASV(3 4)) then it fails as complexASV(1 2)"add" is applied to complexASV(3 4)
-		/*ts
-			complexASV		.(: (
-				(].="real" [.[)
-				(].="imag" [.])
-				(].="mag" [.^2@.+$.^0.5)
-				(].="add" [.(: (~.+$@.complexASV)`).(.$))
-				0/0`
-			)?`).(.$)
-			doubleIntake	:.:
-			module			:.(: ]`).(.$)
-			complex			:.(: (
-				(].="real" [.[)
-				(].="imag" [.])
-				(].="mag" [.^2@.+$.^0.5)
-				0/0`
-			)?`).(.$)
-			solutionA		"mag",3complex4
-			solutionB		complexASV(3 4)"add"(5 6)"real"
-		*/
-		it('complexASV(3 4)"mag"=5', () => expect(complexASV([3, 4])("mag")).eql(5));
-		it(`complexASV(3 4)"real"=3`, () => expect(complexASV([3, 4])("real")).eql(3));
-		it(`"invalid",1complex2=0/0`, () => expect(complex(1, 2)("invalid")).eql(undefined));
-		it(`complexASV(3 4)"add"(5 6)"real"=8`, () => expect(solutionB).eql(8));
-		it(`"real",3complex4=3`, () => expect(complex(3, 4)("real")).eql(3));
-		it(`"mag",3complex4=5`, () => expect(solutionA).eql(5));
-		it('"hello",2module3="hello"', () => expect(module(2, 3)("hello")).eql("hello"));
-		it("(5 6),3doubleIntake4", () => expect(doubleIntake(3, 4)([5, 6])).eql([[3, 4], [5, 6]]));
-	});
-
-	describe("2.59 unionSet +.removeDuplicates", () => {
-		/*ts
-			removeDuplicates			;/.\.(].[)@
-			unionSet					+.removeDuplicates
-		*/
-		it("(1 2 3)unionSet(2 3 4)=(1 2 3 4)", () => expect(unionSet([1, 2, 3], [2, 3, 4])).eql([1, 2, 3, 4]));
-	});
-
-	describe("2.40 cartesianProduct :,(:@ (:~)@)._.(.$).(.$)@.{ removeDuplicates ;/.\\.(].[)@ intersectionSet cartesianProduct.removeDuplicates.((=$ 1`) ()`)??.[@", () => {
-		/*ts
-			pairMap						:@
-			cartesianProduct			:,(:@ (:~)@)._.(.$).(.$)@.{
-			solution					(1 2)cartesianProduct(4 5 6)
-			removeDuplicates			;/.\.(].[)@
-			solutionB					removeDuplicates((1 2) (3 4) (1 2) (5 6))
-			intersectionSet				cartesianProduct.removeDuplicates.((=$ 1`) ()`)??.[@
-		*/
-		it("(1 2 3)intersectionSet(2 3 4)=(2 3)", () => expect(intersectionSet([1, 2, 3], [2, 3, 4])).eql([2, 3]));
-		it("removeDuplicates((1 2) (3 4) (1 2) (5 6))=((1 2) (3 4) (5 6))", () => expect(solutionB).eql([[1, 2], [3, 4], [5, 6]]));
-		it("(1 2)cartesianProduct(4 5 6)=((1 4) (1 5) (1 6) (2 4) (2 5) (2 6))", () => expect(cartesianProduct([1, 2], [4, 5, 6])).eql([[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6]]));
-	});
-
-	describe("2.37 dotProduct :.~.*$@.+$ matrixXVector :,(; dotProduct)._.@$ matrixXMatrix :,(; ~),(; matrixXVector)._.@$", () => {
-		/*ts
-			dotProduct		:.~.*$@.+$
-			matrixXVector	:,(; dotProduct)._.@$
-			matrixXMatrix	:,(; ~),(; matrixXVector)._.@$
-		*/
-		it("((0 1 2) (1 2 3) (2 3 4))matrixXMatrix((2 1 3) (3 2 2) (1 3 1))=((5 8 4) (11 14 10) (17 20 16))", () => expect(matrixXMatrix([[0, 1, 2], [1, 2, 3], [2, 3, 4]], [[2, 1, 3], [3, 2, 2], [1, 3, 1]])).eql([[5, 8, 4], [11, 14, 10], [17, 20, 16]]))
-		it("((0 1 2) (1 2 3) (2 3 4))matrixXVector(0 1 2)=(5 8 11)", () => expect(matrixXVector([[0, 1, 2], [1, 2, 3], [2, 3, 4]],[0, 1, 2])).eql([5, 8, 11]));
-		it("(0 1 2 3)dotProduct(0 1 2 3)=14", () => expect(dotProduct([0, 1, 2, 3], [0, 1, 2, 3])).eql(14));
-	});
-
-	describe("2.36 accumulateN ~.+$@", () => {
-		/*ts
-			accumulateN			~.+$@
-		*/
-		it("accumulateN((1 2 3) (4 5 6) (7 8 9) (10 11 12))=(22 26 30)", () => expect(accumulateN([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])).eql([22, 26, 30]));
-	});
-
-	// describe('treeMap :.(.(.((}.="A")` [.treeMap) [).? ]).@$', () => {
+	// describe('complex numbers - revealing module .(: ((].="real" [.[) (].="imag" [.]) (].="mag" [.^2@.+$.^0.5) (].="add" [.(: (~.+$@.complexASV)`).(.$)) 0/0`)?`).(.$)', () => {
+	// 	// NB if we pass a module here eg, complexASV(1 2)"add"(complexASV(3 4)) then it fails as complexASV(1 2)"add" is applied to complexASV(3 4)
 	// 	/*ts
-	// 		treeMap				:.(.(.((}.="A")` [.treeMap) [).? ]).@$
-	// 		solution			^2treeMap(1 (2 (3 4) 5) (6 7))
+	// 		complexASV		.(: (
+	// 			(].="real" [.[)
+	// 			(].="imag" [.])
+	// 			(].="mag" [.^2@.+$.^0.5)
+	// 			(].="add" [.(: (~.+$@.complexASV)`).(.$))
+	// 			0/0`
+	// 		)?`).(.$)
+	// 		doubleIntake	:.:
+	// 		module			:.(: ]`).(.$)
+	// 		complex			:.(: (
+	// 			(].="real" [.[)
+	// 			(].="imag" [.])
+	// 			(].="mag" [.^2@.+$.^0.5)
+	// 			0/0`
+	// 		)?`).(.$)
+	// 		solutionA		"mag",3complex4
+	// 		solutionB		complexASV(3 4)"add"(5 6)"real"
 	// 	*/
-	// 	it("^2treeMap(1 (2 (3 4) 5) (6 7))=(1 (4 (9 16) 25) (36 49))", () => expect(solution).eql([1, [4, [9, 16], 25], [36, 49]]));
+	// 	it('complexASV(3 4)"mag"=5', () => expect(complexASV([3, 4])("mag")).eql(5));
+	// 	it(`complexASV(3 4)"real"=3`, () => expect(complexASV([3, 4])("real")).eql(3));
+	// 	it(`"invalid",1complex2=0/0`, () => expect(complex(1, 2)("invalid")).eql(undefined));
+	// 	it(`complexASV(3 4)"add"(5 6)"real"=8`, () => expect(solutionB).eql(8));
+	// 	it(`"real",3complex4=3`, () => expect(complex(3, 4)("real")).eql(3));
+	// 	it(`"mag",3complex4=5`, () => expect(solutionA).eql(5));
+	// 	it('"hello",2module3="hello"', () => expect(module(2, 3)("hello")).eql("hello"));
+	// 	it("(5 6),3doubleIntake4", () => expect(doubleIntake(3, 4)([5, 6])).eql([[3, 4], [5, 6]]));
 	// });
+
+	// describe("2.59 unionSet +.removeDuplicates", () => {
+	// 	/*ts
+	// 		removeDuplicates			;/.\.(].[)@
+	// 		unionSet					+.removeDuplicates
+	// 	*/
+	// 	it("(1 2 3)unionSet(2 3 4)=(1 2 3 4)", () => expect(unionSet([1, 2, 3], [2, 3, 4])).eql([1, 2, 3, 4]));
+	// });
+
+	// describe("2.40 cartesianProduct :,(:@ (:~)@)._.(.$).(.$)@.{ removeDuplicates ;/.\\.(].[)@ intersectionSet cartesianProduct.removeDuplicates.((=$ 1`) ()`)??.[@", () => {
+	// 	/*ts
+	// 		pairMap						:@
+	// 		cartesianProduct			:,(:@ (:~)@)._.(.$).(.$)@.{
+	// 		solution					(1 2)cartesianProduct(4 5 6)
+	// 		removeDuplicates			;/.\.(].[)@
+	// 		solutionB					removeDuplicates((1 2) (3 4) (1 2) (5 6))
+	// 		intersectionSet				cartesianProduct.removeDuplicates.((=$ 1`) ()`)??.[@
+	// 	*/
+	// 	it("(1 2 3)intersectionSet(2 3 4)=(2 3)", () => expect(intersectionSet([1, 2, 3], [2, 3, 4])).eql([2, 3]));
+	// 	it("removeDuplicates((1 2) (3 4) (1 2) (5 6))=((1 2) (3 4) (5 6))", () => expect(solutionB).eql([[1, 2], [3, 4], [5, 6]]));
+	// 	it("(1 2)cartesianProduct(4 5 6)=((1 4) (1 5) (1 6) (2 4) (2 5) (2 6))", () => expect(cartesianProduct([1, 2], [4, 5, 6])).eql([[1, 4], [1, 5], [1, 6], [2, 4], [2, 5], [2, 6]]));
+	// });
+
+	// describe("2.37 dotProduct :.~.*$@.+$ matrixXVector :,(; dotProduct)._.@$ matrixXMatrix :,(; ~),(; matrixXVector)._.@$", () => {
+	// 	/*ts
+	// 		dotProduct		:.~.*$@.+$
+	// 		matrixXVector	:,(; dotProduct)._.@$
+	// 		matrixXMatrix	:,(; ~),(; matrixXVector)._.@$
+	// 	*/
+	// 	it("((0 1 2) (1 2 3) (2 3 4))matrixXMatrix((2 1 3) (3 2 2) (1 3 1))=((5 8 4) (11 14 10) (17 20 16))", () => expect(matrixXMatrix([[0, 1, 2], [1, 2, 3], [2, 3, 4]], [[2, 1, 3], [3, 2, 2], [1, 3, 1]])).eql([[5, 8, 4], [11, 14, 10], [17, 20, 16]]))
+	// 	it("((0 1 2) (1 2 3) (2 3 4))matrixXVector(0 1 2)=(5 8 11)", () => expect(matrixXVector([[0, 1, 2], [1, 2, 3], [2, 3, 4]],[0, 1, 2])).eql([5, 8, 11]));
+	// 	it("(0 1 2 3)dotProduct(0 1 2 3)=14", () => expect(dotProduct([0, 1, 2, 3], [0, 1, 2, 3])).eql(14));
+	// });
+
+	// describe("2.36 accumulateN ~.+$@", () => {
+	// 	/*ts
+	// 		accumulateN			~.+$@
+	// 	*/
+	// 	it("accumulateN((1 2 3) (4 5 6) (7 8 9) (10 11 12))=(22 26 30)", () => expect(accumulateN([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])).eql([22, 26, 30]));
+	// });
+
+	// // describe('treeMap :.(.(.((}.="A")` [.treeMap) [).? ]).@$', () => {
+	// // 	/*ts
+	// // 		treeMap				:.(.(.((}.="A")` [.treeMap) [).? ]).@$
+	// // 		solution			^2treeMap(1 (2 (3 4) 5) (6 7))
+	// // 	*/
+	// // 	it("^2treeMap(1 (2 (3 4) 5) (6 7))=(1 (4 (9 16) 25) (36 49))", () => expect(solution).eql([1, [4, [9, 16], 25], [36, 49]]));
+	// // });
 
 	describe(`2.30 squareTree ((}.="A" squareTree) ^2)?@`, () => {
 		/*ts
@@ -110,9 +110,9 @@ export default () => {
 		it("sameParity(2 3 4 5 6 7)=(2 4 6)", () => expect(sameParity([2, 3, 4, 5, 6, 7])).eql([2, 4, 6]));
 	});
 
-	describe("compose .~", () => {
+	describe("compose ~.", () => {
 		/*ts
-			compose		.~
+			compose		~.
 		*/
 		it("6,^2compose+1=49", () => expect(compose(x => x * x, x => x + 1)(6)).eql(49));
 	});
