e64ffa7b92be1dc46fc79fb216a303e00dc78c1e
Darryl <dwsmorris@hotmail.com>
2023-05-07 22:17:18 +0100

    Add generated tutorial build

--- test/logic/main.js
+++ test/logic/main.js
@@ -4,7 +4,8 @@ import generators from "./generators.js";
 import sicp from "./sicp.js";
 import operators from "./operators.js";
 import parser from "./parser.js";
-// import problems from "./problems.js";
+import problems from "./problems.js";
+import temp from "./temp.js";
 
 mocha.setup('bdd');
 
@@ -12,6 +13,7 @@ generators();
 sicp();
 operators();
 parser();
-// problems();
+problems();
+temp();
 
 mocha.run();
---
+++ test/logic/temp.js
@@ -0,0 +1,13 @@
+import ts from "tacitscript";
+
+const {expect} = chai;
+
+export default () => {
+	describe("", () => {
+		/*ts
+
+		*/
+	});
+
+
+};
