9bc11594f30eae9d14d10338e716360ec4266183
Darryl <dwsmorris@hotmail.com>
2023-05-21 23:34:10 +0100

    add problems

--- test/logic/problems.js
+++ test/logic/problems.js
@@ -5,6 +5,56 @@ const {expect} = chai;
 export default () => {
 	describe("Problems", () => {
 
+		describe("281", () => {
+			/*ts
+				isValidCharST           @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$"
+				checkDanglingQuoteAA    ((].="q" ,(; "e"`)) ;)?
+				handleQuoteMarkAA       ((].="q" .(+$ ""`)) .(+$ "q"`))?
+				handleNumberAA          ((].="n" ;) (].="w" ;) .(+$ "n"`))?
+				handleWordAA            ((].="w" ;) .(+$ "w"`))?
+				parseCharASA            :.(
+											([.].="e" [)
+											(].="\"" [.handleQuoteMarkAA)
+											([.].="q" [)
+											(].=" " [.(+$ ""`))
+											(].@"0123456789" [.handleNumberAA)
+											(].@",;:()" [.(+$ "p"`))
+											(].@">=<*+-/^" .([.+$ ]))
+											(].isValidCharST.! [.(+$ "e"`))
+											[.handleWordAA
+										)?
+				operatorsA              (">=" "<=" "<>" ">" "<" "=" "+" "-" "*" "/" "^")
+				replaceA                (.(; "o"`).@)@operatorsA
+				parseLineSS             ""%.(parseCharASA ("" ""))$.checkDanglingQuoteAA.+$.(,` ;).$replaceA
+				solutionSS              "\n"%.parseLineSS@." "$
+				exampleS                solutionSS"10 INPUT \"What is your name: \"; U$
+20 PRINT \"Hello \"; U$
+30 INPUT \"How many stars do you want: \"; N
+40 S$ = \"\"
+50 FOR I = 1 TO N
+60 S$ = S$ + \"*\"
+70 NEXT I
+80 PRINT S$
+90 INPUT \"Do you want more stars? \"; A$
+100 IF LEN(A$) = 0 THEN GOTO 90
+110 A$ = LEFT$(A$, 1)
+120 IF A$ = \"Y\" OR A$ = \"y\" THEN GOTO 30
+130 PRINT \"Goodbye \"; U$
+140 END"
+			*/
+			it("exampleS", () => expect(exampleS).eql("nwqpw nwqpw nwqpw nwoq nwwonww nwowoq nww nww nwqpw nwwpwponwwn nwowpwpnp nwwoqwwoqwwn nwqpw nw"))
+		});
+
+		describe("294", () => {
+			/*ts
+				alphabetS           "abcdefghijklmnopqrstuvwxyz"
+				offsetSA            ""%.(@alphabetS.[)@
+				solutionSS          "\n"%.offsetSA@.([ .([.# .(].` #@./$.]).^$.+$).%$.[).*$.(+$.%26.'alphabetS)@.+$
+				exampleS            solutionSS"attackatdusk
+cab"
+			*/
+			it("solved", () => expect(exampleS).eql("ctucclctewsl"));
+		});
 
 // 		describe("323", () => {
 // 			/*ts
