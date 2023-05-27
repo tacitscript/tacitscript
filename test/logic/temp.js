60bce1b9abf893031a8237bb8588c066de0b3f31
Darryl <dwsmorris@hotmail.com>
2023-05-27 19:54:07 +0100

    update logo

--- test/logic/temp.js
+++ test/logic/temp.js
@@ -3,11 +3,25 @@ import ts from "tacitscript";
 const {expect} = chai;
 
 export default () => {
-	describe("", () => {
-		/*ts
+    describe("", () => {
+        /*ts
 
-		*/
-	});
-
+        */
+    });
+    
+    // describe("generators", () => {
+    // 	/*ts
+    // 		naturals				(].+1)^(1 )
+    // 		firstThree				3%.{
+    // 		firstThreeEven			(%2.=0)?.3%.{
+    // 		firstThreeEvenSquares	^2@.(%2.=0)?.3%.{
+    // 		fib						(_2%.].+$)^(1 1)
+    // 		firstSix				6%.{
+    // 	*/
+    // 	it("firstThree", () => expect(firstThree(naturals)).eql([1, 2, 3]));
+    // 	it("firstThreeEven", () => expect(firstThreeEven(naturals)).eql([2, 4, 6]));
+    // 	it("firstThreeEvenSquares", () => expect(firstThreeEvenSquares(naturals)).eql([4, 16, 36]));
+    // 	it("firstSixFib", () => expect(firstSix(fib)).eql([1, 1, 2, 3, 5, 8]));
+    // });
 
 };
