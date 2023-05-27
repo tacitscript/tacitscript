6b69ed776b049b451ee2c5fd0bbec03629ca8165
Darryl <dwsmorris@hotmail.com>
2023-05-27 19:54:46 +0100

    Revert "update logo"
    This reverts commit 60bce1b9abf893031a8237bb8588c066de0b3f31.

--- test/logic/main.js
+++ test/logic/main.js
@@ -1,19 +1,17 @@
 const {expect} = chai;
 
-// import generators from "./generators.js";
-// import sicp from "./sicp.js";
-// import operators from "./operators.js";
-// import parser from "./parser.js";
+import generators from "./generators.js";
+import sicp from "./sicp.js";
+import operators from "./operators.js";
+import parser from "./parser.js";
 // import problems from "./problems.js";
-import temp from "./temp.js";
 
 mocha.setup('bdd');
 
-// generators();
-// sicp();
-// operators();
-// parser();
+generators();
+sicp();
+operators();
+parser();
 // problems();
-temp();
 
 mocha.run();
