158c9bb59f38a2bcc4daecddd2cd3af1003011e9
Darryl <dwsmorris@hotmail.com>
2023-06-04 00:32:43 +0100

    update rosetta code

--- test/logic/rosetta.js
+++ test/logic/rosetta.js
@@ -4,8 +4,8 @@ const {expect} = chai;
 
 export default () => {
 
-    // tacitscript is a pure language.
-    // Where the question requests user input or display output, the example will interpret these as functional inputs and outputs.
+    // tacitscript is a pure language (embedded DSL.)
+    // Where the question requests user input and to display/print output, the example will interpret these as functional inputs and outputs.
 
     describe("1. Compare the length of two strings", () => {
         /*ts
@@ -57,11 +57,12 @@ export default () => {
         it("1", () => expect(exampleA).eql([0, 1, 1]));
     });
 
-    describe("6. String append", () => {
+    describe("6. String concatenation", () => {
         /*ts
-            solutionS       "Hello"+", world!"         tacitscript has no mutation or += style string concatenation
+            startS          "Hello"                 strings are immutable
+            completeS       startS+", world!"       equals "Hello, world!"
         */
-        it("1", () => expect(solutionS).eql("Hello, world!"));
+        it("1", () => expect(completeS).eql("Hello, world!"));
     });
 
     describe("7. String comparison", () => {
@@ -72,5 +73,56 @@ export default () => {
         */
         it("1", () => expect(exampleA).eql([0, 1, 0, 1, 1]));
     });
+
+    describe("8. String interpolation (included)", () => {
+        /*ts
+            textS           "little"
+            solutionS       ("X" textS)@"Mary had a X lamb"
+        */
+        it("1", () => expect(solutionS).eql("Mary had a little lamb"));
+    });
+
+    describe("9. String matching", () => {
+        /*ts
+            solutionSSA         :._.(@$.([.=0 ;) _@.@$.([.=0 )).+$        second result below is truthy and shows match indices
+            exampleA            "ababc"solutionSSA"ab"                    equals (1 (0 2) 0)
+        */
+        it("1", () => expect(exampleA).eql([1, [0, 2], 0]));
+    });
+
+    describe("10. Substring", () => {
+        /*ts
+            knownCharS          "b"
+            knownSubstringS     "cd"
+            solutionASA         :.(
+                                    %$.1'
+                                    ,([ ;).%$.]
+                                    ]._1%.[
+                                    .(.(].knownCharS@.[ [.]) ]).%$.1'
+                                    .(.(].knownSubstringS@.[ [.]) ]).%$.1'
+                                )
+            exampleA            (3 4)solutionASA"abcdefgh"                    equals ("defg" "defgh" "abcdefg" "bcde" "cdef")
+        */
+        it("1", () => expect(exampleA).eql(["defg", "defgh", "abcdefg", "bcde", "cdef"]));
+    });
+
+    describe("11. Conditional structures", () => {
+        /*ts
+            condNS      (
+                            (<0 +" is negative")
+                            (=0 +" is zero")
+                            ""+.+" is positive"
+                        )?
+            exampleS    condNS3                     equals "3 is positive"
+        */
+        it("1", () => expect(exampleS).eql("3 is positive"));
+    });
+
+    describe("12. Loops/While", () => {
+        /*ts
+            solutionN       >0^({"console.log">./2.[)1024       console log entries: 1024 512 256 128 64 32 16 8 4 2 1
+        */
+        it("1", () => expect(solutionN).eql(0));
+    })
 
 };
