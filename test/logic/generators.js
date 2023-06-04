51e51d945d24dbb81aa70877f5a49f3c7f632bcd
Darryl <dwsmorris@hotmail.com>
2023-06-04 20:20:17 +0100

    updates to tests

--- test/logic/generators.js
+++ test/logic/generators.js
@@ -3,6 +3,22 @@ import ts from "tacitscript";
 import {streamFromArray, naturals} from "utilities";
 
 export default () => {
+
+    // describe("generators", () => {
+    // 	/*ts
+    // 		naturals				(].+1)^(1 )
+    // 		firstThree				3%.{
+    // 		firstThreeEven			(%2.=0)?.3%.{
+    // 		firstThreeEvenSquares	^2@.(%2.=0)?.3%.{
+    // 		fib						(_2%.].+$)^(1 1)
+    // 		firstSix				6%.{
+    // 	*/
+    // 	it("firstThree", () => expect(firstThree(naturals)).eql([1, 2, 3]));
+    // 	it("firstThreeEven", () => expect(firstThreeEven(naturals)).eql([2, 4, 6]));
+    // 	it("firstThreeEvenSquares", () => expect(firstThreeEvenSquares(naturals)).eql([4, 16, 36]));
+    // 	it("firstSixFib", () => expect(firstSix(fib)).eql([1, 1, 2, 3, 5, 8]));
+    // });
+
 	describe("js primes", () => {
 		const take = n => function*(iterable) {
 			let i = 0;
