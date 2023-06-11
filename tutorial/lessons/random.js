1a2285a45a35c696d97822f113df25b36431ccff
Darryl <dwsmorris@hotmail.com>
2023-06-11 13:37:23 +0100

    update random number lesson

--- tutorial/lessons/random.js
+++ tutorial/lessons/random.js
@@ -10,9 +10,9 @@ export default {
 	name: "Random Numbers",
 	operations: <React.Fragment><a href="#random">(?) random</a></React.Fragment>,
 	description: <div>
-		<p>The <a href="#random">(?) random</a> operation takes two numbers (<b>left</b> and <b>right</b>) and returns a random number in the range [<b>left</b>, <b>right</b>) ie. the range includes <b>left</b> but excludes <b>right</b>.</p>
+		<p>The <a href="#random">(?) random</a> operation takes a number <b>n</b> to the right and, when called with any left argument, returns a random floating point number in the range [<b>0</b>, <b>n</b>) ie. the range includes <b>0</b> but excludes <b>n</b>.</p>
 		<div className="code-block">{getOperationExamples([
-			["randomN", "0?1", "0(<|=)randomN<1"],
+			["randProportionXN", "?1", "0(<|=)(randProportionXN6)<1"],
 		])}</div>
 		<p>Note that <a href="#random">(?) random</a> is <b>not</b> <a href="https://en.wikipedia.org/wiki/Referential_transparency" target="_blank">referentially transparent</a>.</p>
 	</div>,
@@ -21,10 +21,10 @@ export default {
 		getJs: ({def}) => `const solution = /*ts ${def} */;`,
 		getHtml: details => <div className="single-line name-expression">
 			<div className="name">shuffleAA</div>
-			<TextEdit {...{...details, multiline: true, solution: '(0`.?1)<'}}/>
+			<TextEdit {...{...details, multiline: true, solution: '?1<'}}/>
 		</div>,
 		getTestValues: () => [start],
-		hint1: "Use operators: . ` < ?",
+		hint1: "Use operators: < ?",
 		hint2: "Sort by generating a different random number for each element",
 		tests: [
 			{description: testValue => <span><b>shuffleAA</b>{`${ts.toString(testValue)} contains the values 1 thru 9 in random order`}</span>, condition: ({solution, testValue}) => isShuffled(solution(testValue))},
